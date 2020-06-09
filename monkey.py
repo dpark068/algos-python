127f4023c739a02fde65034c627f1c3fc4ca7463
Daniel Park <dpark068@gmail.com>
2020-06-09 17:05:44 -0400

    arrange docs

--- monkey.py
+++
@@ -1,7 +0,0 @@
-from fractions import Fraction
-
-def numerator(num):
-    frac = Fraction(num)
-    return frac.numerator
-
-print(numerator(0.75))
---
+++ playground.py
@@ -0,0 +1,2 @@
+dp = [[[0] * 7] for _ in range(3)]
+print(dp[0][0])
